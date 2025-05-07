9aa10f0fe200905281a47445a2a099f3d0d82895
Rachit Saxena <rachitmanit@gmail.com>
2025-05-07 18:42:10 +0530

    Machine Coding Problems

--- Hands-on Problems/1. Logging Framework/OutputHandler/FileOutputHandler.py
+++ Hands-on Problems/1. Logging Framework/OutputHandler/FileOutputHandler.py
@@ -1,21 +1,50 @@
+import datetime
+import os.path
+import time
+
 from .AbstractOutputHandler import AbstractOutputHandler
 
 class FileOPHandler(AbstractOutputHandler):
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, threshold=10240, rotation_check_interval=5):
         self.file_name = file_name
+        self.threshold = threshold
+        self.rotation_check_interval = rotation_check_interval
+        self.last_check_time = time.time()
         try:
             self._filehandler = open(self.file_name, 'a+')
         except Exception as exp:
-            print("Error opening file {} by FileOPHandler".format(self.file_name))
+            print("Error opening file {} by FileOPHandler. Exception: {}".format(self.file_name, exp))
             self._filehandler = None
 
     def write(self, text):
         try:
-            if self.file_name:
+            if self._filehandler:
                 self._filehandler.writelines(text + "\n")
         except Exception as exp:
             print("Failed to write message in file {}".format(self.file_name))
+            print(f'Outputting to Console Log: {text}')
+
+        if time.time() - self.last_check_time > self.rotation_check_interval:
+            self.log_rotate()
+            self.last_check_time = time.time()
+
+    def log_rotate(self):
+        if os.path.exists(self.file_name) and os.path.getsize(self.file_name) / 1024 > self.threshold:
+            if self._filehandler:
+                self._filehandler.close()
+
+            timestamp = time.strftime("%Y%m%d_%H%M%S")
+            rotated_file_name = self.file_name + f'_{timestamp}'
+
+            try:
+                os.rename(self.file_name, rotated_file_name)
+                print(f'Rotated {self.file_name} to {rotated_file_name}')
+                self._filehandler = open(self.file_name, 'a+')
+            except Exception as exp:
+                print(f'Failed to open file {self.file_name} by FileIOHandler. Exception: {exp}')
+                self._filehandler = None
+
 
     def close(self):
         if self._filehandler:
--- Hands-on Problems/1. Logging Framework/main.py
+++ Hands-on Problems/1. Logging Framework/main.py
@@ -1,9 +1,12 @@
+import os.path
+from traceback import print_tb
+
 from Logger.LogLevel import LogLevel
 from Logger.Logger import Logger
 from OutputHandler.FileOutputHandler import FileOPHandler
 from OutputHandler.ConsoleOutputHandler import ConsoleOPHandler
 
-if __name__ == '__main__':
+def test_console_output_with_updates():
     logger = Logger(ConsoleOPHandler(), LogLevel.DEBUG)
     logger.debug("Debug Message1")
     logger.info("Info Message1")
@@ -19,7 +22,9 @@ if __name__ == '__main__':
     logger.error("Error Message2")
 
     print("------------------------------------")
-    logger = Logger(ConsoleOPHandler(), LogLevel.INFO)
+
+def test_file_output():
+    # Below should not initialise logger again.
     logger = Logger(ConsoleOPHandler(), LogLevel.WARN)
 
     print("------------------------------------")
@@ -37,3 +42,28 @@ if __name__ == '__main__':
     logger.warn("Warn Message4")
     logger.error("Error Message4")
 
+def test_case_test_log_rotate():
+    logger = Logger(ConsoleOPHandler(), LogLevel.WARN)
+    logger.set_output_handler(FileOPHandler("log.txt", rotation_check_interval=2))
+    counter = 0
+    while True:
+        counter += 1
+        logger.debug("Debug Message{}".format(counter))
+        logger.info("Info Message{}".format(counter))
+        logger.warn("Warn Message{}".format(counter))
+        logger.error("Error Message{}".format(counter))
+
+if __name__ == '__main__':
+    # Test 1
+    test_console_output_with_updates()
+
+    # Test 2
+    test_file_output()
+
+    # Test 3
+    # test_case_test_log_rotate()
+
+
+    # print(os.path.getsize("log.txt") / 1024**2 )
+    # print(os.path.getsize("log.txt_20250507_183858") / 1024**2 )
+    # print(os.path.getsize("log.txt_20250507_183906") / 1024**2 )
